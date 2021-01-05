332be74f5e41072ae40608f4bceb7c7ff0fd0325
VanjaRo <vanjaro43@gmail.com>
2021-01-05 22:25:01 +0300

    Hello

---
+++ api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
+
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
