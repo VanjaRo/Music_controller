03dacf4ff2cd20c77e5ecc0962e6b629ec3b2d3d
VanjaRo <vanjaro43@gmail.com>
2021-01-13 17:49:20 +0300

    Added play/pause functionality for a room.

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -12,7 +12,25 @@ export default class MusicPlayer extends Component {
   constructor(props) {
     super(props);
   }
+  pauseSong() {
+    const requestOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    fetch("/spotify/pause", requestOptions);
+  }
 
+  playSong() {
+    const requestOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    fetch("/spotify/play", requestOptions);
+  }
   render() {
     const songProgress = (this.props.time / this.props.duration) * 100;
     return (
@@ -29,7 +47,11 @@ export default class MusicPlayer extends Component {
               {this.props.artists}
             </Typography>
             <div>
-              <IconButton>
+              <IconButton
+                onClick={() => {
+                  this.props.is_playing ? this.pauseSong() : this.playSong();
+                }}
+              >
                 {this.props.is_playing ? <Pause /> : <PlayArrow />}
               </IconButton>
               <IconButton>
